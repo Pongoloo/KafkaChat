f5d5c2fa3b2de44e35fe72715eb569e4af66aa02
Jakub <kubakarwow@gmail.com>
2024-06-13 13:00:16 +0200

    adding chat works, gonna try to implement changing chats

--- src/test/java/pjatk/tpo/ChatWindow.java
+++ src/test/java/pjatk/tpo/ChatWindow.java
@@ -21,6 +21,7 @@ public class ChatWindow extends JFrame {
     private JTextArea chatView;
     private JButton chatSwitchButton;
     private JButton addChatButton;
+    private JTextField newChatNameField;
     private final MessageConsumer messageConsumer;
 
     public ChatWindow(String topic, String id) {
@@ -32,6 +33,7 @@ public class ChatWindow extends JFrame {
         this.setTitle("WOop");
         this.pack();
         ExecutorService executorService = Executors.newFixedThreadPool(2);
+        // READER FUNCTION
         executorService.submit(() -> {
             while(true){
                 messageConsumer.kafkaConsumer.poll(Duration.of(1, ChronoUnit.SECONDS)).forEach(m -> {
@@ -44,6 +46,7 @@ public class ChatWindow extends JFrame {
 
         });
 
+        // SEND MESSAGE FUNCTION
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,6 +54,18 @@ public class ChatWindow extends JFrame {
             }
         });
 
+
+
+        //ADD CHAT FUNCTION
+        addChatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              //  availableChats.addItem(newChatNameField.getText());
+                // now we want to send a special message to everyone so everyone knows about the new chat
+                MessageProducer.send(new ProducerRecord<>("chat","SYSTEM.INFO Created chat:"+newChatNameField.getText()));
+                newChatNameField.setText("");
+            }
+        });
     }
     private void processMessage(String message){
         if(message.startsWith("SYSTEM.INFO Created chat")){
