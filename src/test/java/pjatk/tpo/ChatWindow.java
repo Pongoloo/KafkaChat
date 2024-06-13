b3372e425c601303216e76a92aa55c1fcb3f5f53
Jakub <kubakarwow@gmail.com>
2024-06-13 21:05:21 +0200

    gonna add logging in/out

--- src/test/java/pjatk/tpo/ChatWindow.java
+++ src/test/java/pjatk/tpo/ChatWindow.java
@@ -1,5 +1,6 @@
 package pjatk.tpo;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import javax.swing.*;
@@ -8,12 +9,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class ChatWindow extends JFrame {
     private JPanel mainPanel;
@@ -26,59 +28,88 @@ public class ChatWindow extends JFrame {
     private JButton chatSwitchButton;
     private JButton addChatButton;
     private JTextField newChatNameField;
-    private final MessageConsumer messageConsumer;
-    private List<String> topics;
-
+    private JComboBox activeUsers;
+    private MessageConsumer messageConsumer;
+    private Map<String, List<String>> chats = new HashMap<>();
     public ChatWindow(String topic, String id) {
+        setUpFrame(topic, id);
+        changeGraphics();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        startReading(executorService);
+        addSendingMessages(id);
+        addAddingChats();
+        addSwitchingChats();
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+    private void setUpFrame(String topic, String id) {
         messageConsumer = new MessageConsumer(topic, id);
-        topics= new ArrayList<>();
-        topics.add(topic);
+        chats.put(topic, new ArrayList<>());
         this.availableChats.addItem(topic);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(600, 800));
         this.add(mainPanel);
         this.setVisible(true);
-        this.setTitle("WOop");
+        this.setTitle(id + " CHAT");
         this.pack();
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        // READER FUNCTION
+    }
+
+    private void startReading(ExecutorService executorService) {
         executorService.submit(() -> {
-            while(true){
+            while (true) {
                 messageConsumer.kafkaConsumer.poll(Duration.of(1, ChronoUnit.SECONDS)).forEach(m -> {
-                    System.out.println("on chat:"+m.topic()+" mess:"+m.value());
-                    processMessage(m.value());
-                 //   chatView.append(m.value() + '\n');
-                 //   System.out.println("KURWA KURWA KURWA KURWA KURWA KURWA");
+                    System.out.println("on chat:" + m.topic() + " mess:" + m.value());
+                    processMessage(m);
+                    //   chatView.append(m.value() + '\n');
+                    //   System.out.println("KURWA KURWA KURWA KURWA KURWA KURWA");
                 });
             }
-
         });
-
-        // SEND MESSAGE FUNCTION
-        sendButton.addActionListener(new ActionListener() {
+    }
+    private String formatTime(int time){
+        if(time<10){
+            return "0"+time;
+        } else{
+            return time+"";
+        }
+    }
+    private void addSendingMessages(String id) {
+        System.out.println("we are adding the actionlistener XD");
+        this.sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MessageProducer.send(new ProducerRecord<>(availableChats.getSelectedItem().toString(), id + " " + messageField.getText().strip()));
+                System.out.println("nigga wtf");
+                LocalDateTime now = LocalDateTime.now();
+                String timestamp = formatTime(now.getHour()) + ":" + formatTime(now.getMinute()) + ":" + formatTime(now.getSecond());
+
+                MessageProducer.send(new ProducerRecord<>(availableChats.getSelectedItem().toString(), timestamp + " - " + id + ":" + messageField.getText().strip()));
+                messageField.setText("");
             }
         });
+    }
 
-
-
-        //ADD CHAT FUNCTION
+    private void addAddingChats() {
         addChatButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-              //  availableChats.addItem(newChatNameField.getText());
+                //  availableChats.addItem(newChatNameField.getText());
                 // now we want to send a special message to everyone so everyone knows about the new chat
-                MessageProducer.send(new ProducerRecord<>("chat","SYSTEM.INFO Created chat:"+newChatNameField.getText()));
+                MessageProducer.send(new ProducerRecord<>("chat", "SYSTEM.INFO Created chat:" + newChatNameField.getText()));
                 newChatNameField.setText("");
             }
         });
-        //SWITCHING CHATS FUNCTION
+    }
+
+    private void addSwitchingChats() {
         chatSwitchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                String chatText = chats.get(availableChats.getSelectedItem()).stream().collect(Collectors.joining());
+                chatView.setText(chatText);
 //                messageConsumer.kafkaConsumer.unsubscribe();
 //                System.out.println("unsubscribed");
 //                messageConsumer.kafkaConsumer.ad(Collections.singleton(availableChats.getSelectedItem().toString()));
@@ -87,17 +118,29 @@ public class ChatWindow extends JFrame {
             }
         });
     }
-    private void processMessage(String message){
-        if(message.startsWith("SYSTEM.INFO Created chat")){
-            int chatNameBeginIndex = message.indexOf(":");
-            String topic = message.substring(chatNameBeginIndex + 1);
+
+
+
+    private void changeGraphics() {
+        chatView.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));
+        this.messageField.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));
+    }
+
+    private void processMessage(ConsumerRecord<String, String> message) {
+        if (message.value().startsWith("SYSTEM.INFO Created chat")) {
+            int chatNameBeginIndex = message.value().indexOf(":");
+            String topic = message.value().substring(chatNameBeginIndex + 1);
             availableChats.addItem(topic);
-            topics.add(topic);
+            chats.put(topic, new ArrayList<>());
             // testing subbing to many :D
 
-            messageConsumer.kafkaConsumer.subscribe(topics);
-        } else{
-            chatView.append(message +'\n');
+            messageConsumer.kafkaConsumer.subscribe(chats.keySet());
+        } else {
+            chats.get(message.topic()).add(message.value() + '\n');
+            if (message.topic().equals(availableChats.getSelectedItem())) {
+                chatView.append(message.value() + '\n');
+            }
+
         }
     }
 }
